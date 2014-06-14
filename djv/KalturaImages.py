c975ac5c7ac8bdee679c458f323e2490fe03102c
Alice <alice@valour.me>
2014-06-14 15:57:16 -0400

    in better quality

--- djv/KalturaImages.py
+++ djv/KalturaImages.py
@@ -20,19 +20,19 @@ def generateImages(entry_id):
     r = requests.get(API_BASE_URL+'service=media&action=get&format=1&entryId='+entry_id+'&ks='+ks)
     data = r.json()
     secs = data['duration']
+    w = data['width']
+    h = data['height']
 
     i = 0
     until = secs/3
     while i <= until:
-        imageAtSecond(entry_id, i)
+        req2_url = '{0}/thumbnail/entry_id/{1}/quality/100/vid_sec/{2}/width/{3}/height/{4}'.format(PUBLIC_BASE_URL,entry_id,i,w,h)
+        print req2_url
+        r2 = requests.get(req2_url)
+        f = open('../static/images/'+entry_id+'-'+str(i)+'.jpg','wb')
+        f.write(r2.content)
+        f.close()
         i+=1
 
-
-def imageAtSecond(entry_id, s):
-    r = requests.get(PUBLIC_BASE_URL+'/thumbnail/entry_id/'+entry_id+'/quality/100/vid_sec/'+str(s))
-    print r.headers
-    f = open('../static/images/'+entry_id+'-'+str(s)+'.jpg','wb')
-    f.write(r.content)
-    f.close()
 
 generateImages('1_423rtyb6')
