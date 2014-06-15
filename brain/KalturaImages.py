9d6503f930c2d0df6401a4c230bda2fd5f8339e4
Alice <alice@valour.me>
2014-06-15 03:06:52 -0400

    fixed counting error

--- brain/KalturaImages.py
+++ brain/KalturaImages.py
@@ -23,12 +23,12 @@ def GetKS():
 
 def get_image(entry_id,i):
     req2_url = '{0}/thumbnail/entry_id/{1}/quality/100/vid_sec/{2}/width/800'.format(PUBLIC_BASE_URL,entry_id,i)
+    print req2_url
     r2 = requests.get(req2_url)
     file_location = entry_id+'-'+str(i)+'.jpg'
     f = open(file_location,'w+')
     f.write(r2.content)
     f.close()
-    print 'written to '+file_location
 
 def generate_images(entry_id):
     ks = GetKS()
@@ -39,14 +39,13 @@ def generate_images(entry_id):
     secs = data['duration']
 
     i = 0
-    until = secs/10
-    '''
     with concurrent.futures.ThreadPoolExecutor(max_workers=4) as executor:
-        while i <= until:
+        while i <= secs:
             executor.submit(get_image, entry_id, i)
-            i+=1
+            i+=10
     '''
     while i <= until:
         get_image(entry_id,i)
         i += 1
+    '''
     os.chdir(HERE)
