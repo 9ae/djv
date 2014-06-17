22bf0c3c9b89261f1d8b2c2d4a166cb976d8b365
Henry Chen <chendaizhuo@gmail.com>
2014-06-17 17:52:40 -0400

    Fixed a bug

--- brain/kaltura.py
+++ brain/kaltura.py
@@ -49,7 +49,7 @@ def get_entry_download_url(entry_id):
     ret = get_entry_metadata(entry_id)
     try:
         url = ret['downloadUrl']
-        return URL
+        return url
     except:
         return None
 
