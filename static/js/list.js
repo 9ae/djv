2234730fdae8b331f31dcb58f4f6e172c1a90adb
Alice <alice@valour.me>
2014-06-17 23:33:26 -0400

    uploader in the works

--- static/js/list.js
+++ static/js/list.js
@@ -10,9 +10,11 @@ function makeURL(service,action){
 
 function loadList(){
     var partnerId = $('#partner').val();
-    var url = makeURL('media','list');
+   var url = makeURL('media','list');
     var tags = tagsChanged();
-    url+='&filter:tagsLike='+tags.join(',');
+    if(tags.length>0) {
+        url += '&filter:tagsLike=' + tags.join(',');
+    }
      $('#listContent').html('');
     $.get(url, function(data){
         console.log(data);
@@ -24,21 +26,43 @@ function loadList(){
             a.append(img);
             div.append(a);
             $('#listContent').append(div);
-          /*  var iframe = '<iframe src="http://www.kaltura.com/p/'+partnerId+'/sp/'+partnerId+'00/embedIframeJs' +
-            '/uiconf_id/24675211/partner_id/'+partnerId+'?iframeembed=true&playerId=24675211&entry_id='+media.id+'"' +
-            ' width="400" height="330" allowfullscreen webkitallowfullscreen mozAllowFullScreen frameborder="0"></iframe>';
-            $('#listContent').append(iframe); */
         }
-
     });
 }
 
 function uploadFile(event){
-
+    var file = $('#fileData').val();
+    var token = $('#uploadTokenId').val();
+    if(token==""){
+        return;
+    }
+    var url = makeURL('uploadToken','upload');
+    var obj = {'fileData': file, 'uploadTokenId': token};
+    $.post(url,obj, function(data){
+        console.log('upload to token');
+        var uploadResults = data;
+        if(uploadResults['fileName']!=undefined){
+            $.post(makeURL('media','add'), {'entry:mediaType':2},function(data){
+                console.log('create media');
+                var addMediaResults = data;
+                var linkBody = {'entryId':addMediaResults.id,
+                    'resource:token': token,
+                    'resource:objectType':'KalturaUploadedFileTokenResource'};
+                $.post(makeURL('media','addContent'),linkBody).done(function(){
+                    console.log('upload complete');
+                });
+            });
+        }
+    });
 }
+
 
 window.onload = function(){
     addTag('#tag-warning');
-    $('#upload').attr('action',makeURL('media','upload'));
     $('#first_tag').val();
+
+    var ut = $.get(makeURL('uploadToken','add'),function(data){
+        $('#uploadTokenId').val(data['id']);
+        $('#do_upload').click(uploadFile);
+    });
 };
